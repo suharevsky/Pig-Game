06ce9f7f0c78d38364d5174ac376618d7eb370e0
Suharevsky <aliksui.ua@mail.ru>
2021-01-08 14:28:09 +0200

    rolling dice functionality

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+//Selecting elements
+const score0El = document.getElementById('score--0');
+const score1El = document.querySelector('#score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// Starting conditions
+diceEl.classList.add('hidden');
+score1El.textContent = 0;
+score0El.textContent = 0;
+
+//Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  // 1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  diceEl.classList.remove('hidden');
+  diceEl.setAttribute('src', `dice-${dice}.png`);
+
+  // 2. Display dice
+
+  // 3. Check for rolled 1: if true, switch to next player
+});
+
+//const diceValue = Math.trunc(Math.random(6 * 10));
+
+//console.log(Math.random(6 * 100));
